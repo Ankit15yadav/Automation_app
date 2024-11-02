f0c36873662482870fab21ff97734e5f62de1db8
Ankit15yadav <135987492+Ankit15yadav@users.noreply.github.com>
2024-11-03 00:48:45 +0530

    2 nd commit

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import MenuOptions from '@/components/sidebar'
+import React, { Children } from 'react'
+
+type Props = { children: React.ReactNode }
+
+const Layout = (props: Props) => {
+    return (
+        <div className=' flex overflow-hidden h-screen'>
+            <MenuOptions />
+            <div className=' w-full'>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default Layout
